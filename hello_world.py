be28c1e0a8fd0221ae14b868e3b68e0b43b501ed
Ubuntu <ubuntu@ip-172-31-56-74.ec2.internal>
2020-11-26 01:05:18 +0000

    Hello world with Pybadge

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by: Roman Cernetchi
+# Created on: November 2020
+# This is a "Hello, World!" program on Pybadge
+
+
+def game_scene():
+    # this function shows hello world on Pybadge
+
+    print("\n\n\n")  # 3 blank lines
+    print("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass  # just a placeholder for now
+
+
+if __name__ == "__main__":
+    game_scene()
---
+++ hello_world1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Roman Cernetchi
+# Created on: November 2020
+# This is a "Hello, World!" program on Pybadge
+
+
+def game_scene():
+    # this function shows hello world on Pybadge
+
+    print("\n\n\n")  # 3 blank lines
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    game_scene()
