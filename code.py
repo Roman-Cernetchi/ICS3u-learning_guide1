bfe9eaa594000309c4f5fb4995ff9b1538cd3e94
Roman-Cernetchi <74559770+Roman-Cernetchi@users.noreply.github.com>
2020-12-01 12:23:07 -0500

    Add files via upload

--- code.py
+++ code.py
@@ -12,15 +12,24 @@ def game_scene():
     # this function shows hello world on Pybadge
 
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+    
     background = stage.Grid(image_bank_background, 10, 8)
+    
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
     game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
+    game.layers = [ship] + [background]
     game.render_block()
 
     while True:
-        # repeat forever, or you turn it off
-        pass # just a placeholder for now
+        # get user input
+        
+        # update game logic
+        
+        # redraw Sprites
+        game.render_sprites([ship])
+        game.tick()
 
 
 if __name__ == "__main__":
