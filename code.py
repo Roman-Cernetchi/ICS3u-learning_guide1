793951e45d333a4131f7214b2de85c640a0677c7
Roman-Cernetchi <74559770+Roman-Cernetchi@users.noreply.github.com>
2020-12-07 17:04:14 -0500

    Add files via upload

--- code.py
+++ code.py
@@ -6,7 +6,7 @@
 
 import ugame
 import stage
-
+import constants
 
 def game_scene():
     # this function shows hello world on Pybadge
@@ -14,17 +14,35 @@ def game_scene():
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
     
-    background = stage.Grid(image_bank_background, 10, 8)
+    background = stage.Grid(image_bank_background, constants.SCREEN_X, constants.SCREEN_Y)
     
     ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
-    game = stage.Stage(ugame.display, 60)
+    game = stage.Stage(ugame.display, constants.FPS)
     game.layers = [ship] + [background]
     game.render_block()
 
     while True:
         # get user input
+        keys = ugame.buttons.get_pressed()
         
+        if keys & ugame.K_X:
+            pass
+        if keys & ugame.K_O:
+            pass
+        if keys & ugame.K_START:
+            pass
+        if keys & ugame.K_SELECT:
+            pass
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            pass
+        if keys & ugame.K_DOWN:
+            pass
+     
         # update game logic
         
         # redraw Sprites
